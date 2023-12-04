f56cb04d4ac84ae2b12bd1a06bc54f6ea128b722
misratasci <misratasci@gmail.com>
2023-12-04 18:39:41 +0300

    a

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/04 17:49:51 by mitasci           #+#    #+#             */
-/*   Updated: 2023/12/04 18:37:41 by mitasci          ###   ########.fr       */
+/*   Created: 2023/12/04 18:38:10 by mitasci           #+#    #+#             */
+/*   Updated: 2023/12/04 18:39:40 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		if (((char *)s1)[i] != ((char *)s2)[i])
-		{
-			return (((char *)s1)[i] - ((char *)s2)[i]);
-		}
-		i += 1;
-	}
-	return (0);
-}
