2beb1dd191d06b552981785e84b4c99e2d6e2037
misratasci <misratasci@gmail.com>
2023-12-04 17:22:41 +0300

    new

---
+++ ft_strchr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 17:10:59 by mitasci           #+#    #+#             */
+/*   Updated: 2023/12/04 17:18:49 by mitasci          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+static size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i += 1;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+	int	slen;
+
+	i = 0;
+	slen = ft_strlen(s);
+	while (i <= slen)
+	{
+		if (*s == c)
+		{
+			return ((char *)s);
+		}
+		i += 1;
+		s += 1;
+	}
+	return (NULL);
+}
---
+++ ft_strrchr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 17:19:17 by mitasci           #+#    #+#             */
+/*   Updated: 2023/12/04 17:21:28 by mitasci          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+static size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i += 1;
+	}
+	return (i);
+}
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	slen;
+
+	slen = ft_strlen(s);
+	s += slen;
+	while (slen >= 0)
+	{
+		if (*s == c)
+		{
+			return ((char *)s);
+		}
+		slen -= 1;
+		s -= 1;
+	}
+	return (NULL);
+}
