82d2028b17ddbe0c70feaeed8c3be8b4bf522cb4
misratasci <51489990+misratasci@users.noreply.github.com>
2023-12-09 18:09:01 +0300

    a

---
+++ ft_substr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 17:25:45 by mitasci           #+#    #+#             */
+/*   Updated: 2023/12/09 17:25:45 by mitasci          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	sub = (char *)malloc(4 * len);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i += 1;
+	}
+	return (sub);
+}
