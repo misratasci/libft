0e509def33f9373565b1c27867c979dc51642aca
misratasci <51489990+misratasci@users.noreply.github.com>
2023-12-09 17:24:05 +0300

    a

--- libft.h
+++ libft.h
@@ -31,6 +31,7 @@ void	        *ft_memcpy(void *dst, const void *src, size_t n);
 void	        *ft_memmove(void *dst, const void *src, size_t len);
 void            *ft_memset(void *b, int c, size_t len);
 char        	*ft_strchr(const char *s, int c);
+char			*ft_strdup(const char *s);
 size_t	        ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	        ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	        ft_strlen(const char *s);
