1f162785eeae602ffc1a1ba9e87a9dd90b14a227
misratasci <misratasci@gmail.com>
2023-12-12 15:45:58 +0300

    a

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 16:35:27 by mitasci           #+#    #+#             */
-/*   Updated: 2023/12/12 15:10:43 by mitasci          ###   ########.fr       */
+/*   Updated: 2023/12/12 15:45:04 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	i = 0;
 	dstlen = strlength(dst);
-	while (i < dstsize - dstlen - 1 && src[i] && dstsize != 0)
+	while (i < dstsize - dstlen - 1 && src[i] && dstlen < dstsize)
 	{
 		dst[dstlen + i] = src[i];
 		i += 1;
 	}
 	if (dstsize != 0 && dstlen < dstsize)
 		dst[dstlen + i] = 0;
+	if (dstlen > dstsize)
+		dstlen = dstsize;
 	return (dstlen + strlength(src));
 }
