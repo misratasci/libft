d1cf2f48bb8653df52d716af6942ad88f9b94c91
misratasci <misratasci@gmail.com>
2023-12-12 16:26:56 +0300

    a

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 17:23:03 by mitasci           #+#    #+#             */
-/*   Updated: 2023/12/07 13:16:05 by mitasci          ###   ########.fr       */
+/*   Updated: 2023/12/12 16:26:31 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,25 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	int		c1;
+	int		c2;
 	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
-		if (s1[i] != s2[i])
+		c1 = s1[i];
+		c2 = s2[i];
+		if (c1 < 0)
+			c1 *= -1;
+		if (c2 < 0)
+			c2 *= -1;
+		if (c1 != c2)
 		{
-			return (s1[i] - s2[i]);
+			if (c1 > c2)
+				return (1);
+			else
+				return (-1);
 		}
 		i += 1;
 	}
