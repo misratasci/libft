11b1eee8f74a53240f9e94e8c06d5512a293e87d
misratasci <misratasci@gmail.com>
2023-12-14 13:23:13 +0300

    a

--- ft_substr.c
+++ ft_substr.c
@@ -6,23 +6,36 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 17:25:45 by mitasci           #+#    #+#             */
-/*   Updated: 2023/12/14 12:55:19 by mitasci          ###   ########.fr       */
+/*   Updated: 2023/12/14 13:22:38 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static size_t	strlength(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i += 1;
+	return (i);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	i;
 
-	sub = (char *)malloc(len);
+	if (len > strlength(s))
+		len = strlength(s);
+	sub = (char *)malloc(len + 1);
 	i = 0;
-	while (i < len)
+	while (i < len && start < strlength(s))
 	{
 		sub[i] = s[start + i];
 		i += 1;
 	}
+	sub[i] = 0;
 	return (sub);
 }
