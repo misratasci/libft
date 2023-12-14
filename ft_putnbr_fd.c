62b3b3e750943457179b3ccb76aed6aa491567ce
misratasci <misratasci@gmail.com>
2023-12-14 19:16:00 +0300

    a

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,82 +6,60 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 14:15:13 by mitasci           #+#    #+#             */
-/*   Updated: 2023/12/11 14:15:13 by mitasci          ###   ########.fr       */
+/*   Updated: 2023/12/14 19:15:21 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	strlength(const char *s)
+static size_t	char_count(int n)
 {
 	size_t	i;
 
-	i = 0;
-	while (s[i])
+	i = 1;
+	if (n < 0)
+		return (char_count(-n));
+	while (n > 9)
 	{
-		i += 1;
+		n /= 10;
+		i++;
 	}
 	return (i);
 }
 
-char	*join(char const *s1, char const *s2)
-{
-	char	*str;
-	size_t	i;
-
-	str = (char *)malloc(strlength(s1) + strlength(s2));
-	i = 0;
-	while (i < strlength(s1))
-	{
-		str[i] = s1[i];
-		i += 1;
-	}
-	i = 0;
-	while (i < strlength(s2))
-	{
-		str[strlength(s1) + i] = s2[i];
-		i += 1;
-	}
-	str[strlength(s1) + i] = 0;
-	return (str);
-}
-
-char	*inttoa(int n)
-{
-	char	*s;
-	size_t	i;
-
-	s = (char *)malloc(10);
-	i = 0;
-	if (n < 0)
-	{
-		s[i++] = '-';
-		s[i] = 0;
-		return (join(s, inttoa(-n)));
-	}
-	else if (n >= 10)
-		return (join(inttoa(n / 10), inttoa(n % 10)));
-	else
-	{
-		s[i++] = n + '0';
-		s[i] = 0;
-		return (s);
-	}
-}
-
-void	putstr_fd(char *s, int fd)
+static size_t	tenpow(size_t n)
 {
 	size_t	i;
+	size_t	res;
 
 	i = 0;
-	while (s[i])
-	{
-		write(fd, &s[i], 1);
-		i++;
-	}
+	res = 1;
+	while (i++ < n)
+		res *= 10;
+	return (res);
 }
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	putstr_fd(inttoa(n), fd);
+	size_t	len;
+	char	c;
+
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	len = char_count(n);
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n *= -1;
+	}
+	while (len > 0)
+	{
+		c = n / (tenpow(len - 1)) + '0';
+		n = n % (tenpow(len - 1));
+		write(fd, &c, 1);
+		len--;
+	}
 }
