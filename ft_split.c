5cf26dc921e2ad16344e75e5b866714ae755b7fd
misratasci <misratasci@gmail.com>
2023-12-14 17:21:35 +0300

    a

--- ft_split.c
+++ ft_split.c
@@ -6,100 +6,82 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 21:21:34 by mitasci           #+#    #+#             */
-/*   Updated: 2023/12/14 12:55:42 by mitasci          ###   ########.fr       */
+/*   Updated: 2023/12/14 17:21:29 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-static size_t	strlength(const char *s)
+static size_t	count_words(const char *s, char c)
+{
+	size_t	i;
+	size_t	res;
+
+	if (!s[0])
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		else
+			break ;
+	}
+	res = 0;
+	while (s[i])
+	{
+		if (s[i] != c && (i == 0 || s[i - 1] == c))
+			res++;
+		i++;
+	}
+	return (res);
+}
+
+static size_t	count_letters(const char *s, char c)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i])
-	{
-		i += 1;
-	}
+	while (s[i] != c && s[i] != '\0')
+		i++;
 	return (i);
 }
 
-char	*strdupl(const char *s)
+static char	*write_word(const char *s, char c)
 {
 	char	*str;
 	size_t	i;
 
-	str = (char *)malloc(sizeof(s));
+	str = (char *)malloc(count_letters(s, c) + 1);
 	i = 0;
-	while (s[i])
+	while (s[i] != c && s[i] != '\0')
 	{
 		str[i] = s[i];
-		i += 1;
+		i++;
 	}
 	str[i] = 0;
 	return (str);
-}
-
-char	*trim(char const *s1, char c)
-{
-	char	*str;
-	size_t	start;
-	size_t	end;
-	size_t	i;
-
-	str = (char *)malloc(strlength(s1));
-	start = 0;
-	while (s1[start] == c)
-		start += 1;
-	end = strlength(s1) - 1;
-	while (s1[end] == c)
-		end -= 1;
-	i = 0;
-	while (i < end - start + 1)
-	{
-		str[i] = s1[start + i];
-		i += 1;
-	}
-	str[i] = 0;
-	return (str);
-}
-
-static char	*write_word(const char *s, char *dst, size_t i, char stop_char)
-{
-	size_t	j;
-
-	j = 0;
-	while (s[i] != stop_char && s[i] != 0)
-	{
-		dst[j] = s[i];
-		i += 1;
-		j += 1;
-	}
-	dst[j] = 0;
-	return (dst);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**arr;
 	size_t	i;
-	size_t	word_count;
-	char	*str;
+	size_t	word;
 
-	s = trim(s, c);
+	arr = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
 	i = 0;
-	word_count = 0;
-	arr = (char **)malloc(strlength(s));
-	str = (char *)malloc(strlength(s));
-	while (i <= strlength(s))
+	word = 0;
+	while (s[i])
 	{
-		if (i == 0 || (s[i - 1] == c && s[i] != c))
-			str = write_word(s, str, i, c);
-		if ((s[i] == c && s[i + 1] != c) || s[i] == 0)
-			arr[word_count++] = strdupl(str);
-		i += 1;
+		if (s[i] != c && (i == 0 || s[i - 1] == c))
+		{
+			arr[word] = write_word(s + i, c);
+			word++;
+		}
+		i++;
 	}
-	arr[word_count] = 0;
-	free(str);
+	arr[word] = 0;
 	return (arr);
 }
